57959481d09a5b58d2cbea7e692786880fce8eb4
ricardo.sousa <ricardo.sousa@metait.ca>
2022-05-02 12:14:11 -0300

    separando teste em unidades

--- app/src/test/java/br/com/msmlabs/tdd_leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/msmlabs/tdd_leilao/model/LeilaoTest.java
@@ -7,9 +7,9 @@ import org.junit.Test;
 public class LeilaoTest {
 
     @Test
-    public void getDescricao() {
+    public void getDescricao_QuandoRecebeDescricao_DevolveDescricao() {
         // criar cenário de teste
-        Leilao console = new Leilao("Consoles");
+        Leilao console = new Leilao("Console");
 
         // executar ação esperada
         String descricaoDevolvida = console.getDescricao();
@@ -19,16 +19,17 @@ public class LeilaoTest {
     }
 
     @Test
-    public void getMaiorLance() {
-        //verifica se devolve maior lance com apenas um lance
+    public void getMaiorLance_QuandoRecebeApenasUmLance_DevolveMaiorLance() {
         Leilao console = new Leilao("Console");
         console.propoe(new Lance(new Usuario("Alex"), 200.0));
 
         double maiorLanceDevolvidoDoConsole = console.getMaiorLance();
 
         assertEquals(200.0, maiorLanceDevolvidoDoConsole, 0.0001);
+    }
 
-        //verifica se devolve maior lance com mais de um lance em ordem crescente
+    @Test
+    public void getMaiorLance_QuandoRecebeMaisDeUmLanceEmOrdemCrescente_DevolveMaiorLance(){
         Leilao computador = new Leilao("Computador");
         computador.propoe(new Lance(new Usuario("Alex"), 100.0));
         computador.propoe(new Lance(new Usuario("Fran"), 200.0));
@@ -36,8 +37,10 @@ public class LeilaoTest {
         double maiorLanceDevolvidoDoComputador = computador.getMaiorLance();
 
         assertEquals(200.0, maiorLanceDevolvidoDoComputador, 0.0001);
+    }
 
-        //verifica se devolve maior lance com mais de um lance em ordem decrescente
+    @Test
+    public void getMaiorLance_QuandoRecebeMaisDeUmLanceEmOrdemDecrescente_DevolveMaiorLance(){
         Leilao carro = new Leilao("Carro");
         carro.propoe(new Lance(new Usuario("Alex"), 10000.0));
         carro.propoe(new Lance(new Usuario("Fran"), 9000.0));
